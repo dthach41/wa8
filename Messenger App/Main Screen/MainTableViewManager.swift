7c15bfffb7e6046bf4520e78e9666a5f7418d2c0
jackh4 <jackhuang460@gmail.com>
2024-11-05 14:29:27 -0500

    chat list and messaging working

---
+++ Messenger App/Main Screen/MainTableViewManager.swift
@@ -0,0 +1,75 @@
+//
+//  MainTableViewManager.swift
+//  Messenger App
+//
+//  Created by Jack Huang on 11/5/24.
+//
+
+import Foundation
+import UIKit
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatRooms.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Configs.tableViewChatsID, for: indexPath) as! ChatTableViewCell
+        
+        let chat = chatRooms[indexPath.row]
+        var receivers = ""
+        for name in chat.participantNames {
+            if name != currentUser?.displayName {
+                // add commas between users for group chats
+                receivers.append(name)
+            }
+        }
+        cell.labelReceiverName.text = receivers
+
+        if let lastMessage = chat.lastMessage {
+            cell.labelLastMessage.text = lastMessage
+            cell.labelLastMessage.numberOfLines = 1
+            cell.labelLastMessage.lineBreakMode = .byTruncatingTail
+        } else {
+            cell.labelLastMessage.text = ""
+        }
+        
+        if let lastMessageTime = chat.lastMessageTime {
+            cell.labelLastMessageTime.text = DateFormatter.localizedString(from: lastMessageTime, dateStyle: .none, timeStyle: .short)
+        } else {
+            cell.labelLastMessageTime.text = ""
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let chatViewController = ChatViewController()
+        chatViewController.currentUser = currentUser
+        
+        let chat = chatRooms[indexPath.row]
+        for uid in chat.participants {
+            if uid != currentUser?.uid {
+                database.collection("users").document(uid).getDocument { (document, error) in
+                    if error == nil {
+                        if let document = document, document.exists {
+                            do {
+                                let user = try document.data(as: User.self)
+                                chatViewController.otherUser = user
+                                
+                                self.navigationController?.pushViewController(chatViewController, animated: true)
+                            } catch {
+                                print("Error decoding user data")
+                            }
+                        } else {
+                            print("User does not exist")
+                        }
+                    } else {
+                        print(error)
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ Messenger App/Main Screen/Views/ChatTableViewCell.swift
@@ -0,0 +1,95 @@
+//
+//  ChatTableViewCell.swift
+//  Messenger App
+//
+//  Created by Jack Huang on 11/4/24.
+//
+
+import UIKit
+
+class ChatTableViewCell: UITableViewCell {
+    
+    var wrapperCellView: UIView!
+    var labelReceiverName: UILabel!
+    var labelLastMessage: UILabel!
+    var labelLastMessageTime: UILabel!
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupWrapperCellView()
+        setupLabelReceiverName()
+        setupLabelLastMessage()
+        setupLabelLastMessageTime()
+        
+        initConstraints()
+    }
+    
+    func setupWrapperCellView() {
+        wrapperCellView = UITableViewCell()
+        wrapperCellView.backgroundColor = .white
+//        wrapperCellView.layer.cornerRadius = 4.0
+//        wrapperCellView.layer.shadowColor = UIColor.gray.cgColor
+//        wrapperCellView.layer.shadowOffset = .zero
+//        wrapperCellView.layer.shadowRadius = 2.0
+//        wrapperCellView.layer.shadowOpacity = 0.7
+        wrapperCellView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(wrapperCellView)
+    }
+    
+    func setupLabelReceiverName() {
+        labelReceiverName = UILabel()
+        labelReceiverName.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(labelReceiverName)
+    }
+    
+    func setupLabelLastMessage() {
+        labelLastMessage = UILabel()
+        labelLastMessage.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(labelLastMessage)
+    }
+    
+    func setupLabelLastMessageTime() {
+        labelLastMessageTime = UILabel()
+        labelLastMessageTime.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(labelLastMessageTime)
+    }
+    
+    func initConstraints() {
+        NSLayoutConstraint.activate([
+            wrapperCellView.topAnchor.constraint(equalTo: self.topAnchor,constant: 8),
+            wrapperCellView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
+            wrapperCellView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8),
+            wrapperCellView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8),
+            
+            labelReceiverName.topAnchor.constraint(equalTo: wrapperCellView.topAnchor, constant: 8),
+            labelReceiverName.leadingAnchor.constraint(equalTo: wrapperCellView.leadingAnchor, constant: 8),
+            labelReceiverName.heightAnchor.constraint(equalToConstant: 16),
+            labelReceiverName.widthAnchor.constraint(lessThanOrEqualTo: wrapperCellView.widthAnchor, constant: -70),
+            
+            labelLastMessageTime.topAnchor.constraint(equalTo: wrapperCellView.topAnchor, constant: 8),
+            labelLastMessageTime.trailingAnchor.constraint(equalTo: wrapperCellView.trailingAnchor, constant: -8),
+            labelLastMessageTime.heightAnchor.constraint(equalToConstant: 16),
+            labelLastMessageTime.widthAnchor.constraint(lessThanOrEqualTo: wrapperCellView.widthAnchor, constant: -70),
+
+            labelLastMessage.topAnchor.constraint(equalTo: labelReceiverName.topAnchor, constant: 8),
+            labelLastMessage.leadingAnchor.constraint(equalTo: wrapperCellView.leadingAnchor, constant: 8),
+            labelLastMessage.heightAnchor.constraint(equalToConstant: 16),
+            labelLastMessage.widthAnchor.constraint(lessThanOrEqualTo: wrapperCellView.widthAnchor, constant: -70),
+            
+            wrapperCellView.heightAnchor.constraint(equalToConstant: 72)
+        ])
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
